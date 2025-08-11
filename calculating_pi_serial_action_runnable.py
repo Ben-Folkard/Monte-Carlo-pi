ec4569bcbc4ff9c21f9c65b44895b200d4d2e525
Ben Folkard <bsf509@york.ac.uk>
2025-08-11 17:37:07 +0100

    Can now pass arguments from the command line into the Monte Carlo pi calcluating code

---
+++ calculating_pi_serial_action_runnable.py
@@ -0,0 +1,131 @@
+import numpy as np
+import unittest
+import time
+import argparse
+
+def calc_pi(num_samples = 1e5, seed = 12345):
+    """
+    Implements a serial processing Monte-Carlo approach to
+    calculate pi
+    
+    i.e. it creates looks at the positive quadrant of a unit
+    circle in a unit square and each loop checks whether a
+    normal uniformly randomly generated 2d point is inside
+    the circle or outside of it, and then uses the ratio of
+    the number of points in the circle to the total amount
+    of points sampled to get a value of pi
+    
+    num_samples = Specifies how many data points are sampled
+    seed        = Specifies the random number generator's 
+                  so that the results are repeatable and
+                  seed reproducable
+    pi_collection_rate = The amount of iterations between 
+                  storing the current value of pi
+    """
+    # Sets the RNG's seed
+    np.random.seed(seed)
+
+    # Sets the initial amount of points in the circle to 0
+    num_in_circle = 0
+
+    for i in range(1, int(num_samples)+1):
+        # Checks if the randomly generated 2d point is inside
+        # the unit circle finding the magnitude of the vector
+        if np.linalg.norm(np.random.rand(2)) < 1:
+            num_in_circle += 1
+
+    pi = 4*num_in_circle/num_samples
+    
+    return pi
+
+def display_difference(original_value, new_value):
+    diff = new_value-original_value
+    print(f"Absolute difference   = {diff:.2e}")
+    percentage_diff = diff/original_value
+    print(f"Percentage difference = {percentage_diff:.5%}")
+
+def output_results(pi, num_samples, seed, tilde_length = 60):
+    print("Calculating pi using a Monte-Carlo approach:")
+    print("~"*tilde_length)
+    print("Initial variables:")
+    print(f"Max number of samples = {num_samples:,.0f}")
+    print(f"RNG seed              = {seed:,}")
+    print("~"*tilde_length)
+    print(f"Actual value of pi = {np.pi}\n")
+    print(f"Final calculated value of pi = {pi}")
+    print("Difference between pi and calculated value of pi:")
+    display_difference(np.pi, pi)
+
+    print("~"*tilde_length)
+
+class Test_Pi_Calculation(unittest.TestCase):
+    """
+    This class contains the unit tests
+    """
+    def test_calc_pi_basic(self):
+        """
+        A basic test to see that the returned values of pi
+        are outputting values in the expected range             
+        """
+        pi = calc_pi(num_samples=1000, seed=42)
+        
+        self.assertTrue(2.5 < pi < 4.0)
+
+    def test_calc_pi_with_seed(self):
+        """
+        Tests that calc_pi returns consistent results with same seed
+        """
+        pi1 = calc_pi(num_samples=1000, seed=123)
+        pi2 = calc_pi(num_samples=1000, seed=123)
+
+        self.assertEqual(pi1, pi2)
+
+def parallel_vs_serial_benchmark():
+    """
+    Times the difference between the calc_pi with and without
+    parallelisation (using njit)
+    """
+    import time
+    start = time.time()
+    calc_pi(1e7)
+    print("With Numba:", time.time() - start)
+
+    start = time.time()
+    calc_pi_serial(1e7)
+    
+    print("Without Numba:", time.time() - start)
+
+if __name__ == "__main__":
+    """
+    Inputs:
+    num_samples = Specifies how many data points are sampled.
+    seed        = Specifies the random number generator's seed
+                  so that the results are repeatable and
+                  reproducable
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_samples", type=float, default=1e5)
+    parser.add_argument("--seed", type=int, default=12345)
+    args = parser.parse_args()
+
+    num_samples = args.num_samples
+    seed = args.seed
+
+    start = time.time()
+    pi = calc_pi(num_samples=num_samples, seed=seed)
+    print(f"Elapsed = {time.time() - start:.4f} s")
+
+    # Outputs the results to the console
+    output_results(pi, num_samples, seed)
+
+    # Run the unit tests
+    print("\nRunning unit tests...")
+    unittest.main()
+
+    # Alternative unittest syntaxes #
+
+    #unittest.main(argv=['first-arg-is-ignored'], exit=False)
+
+    #runner = unittest.TextTestRunner()
+    #suite = unittest.TestLoader().loadTestsFromTestCase(Test_Pi_Calculation)
+    #runner.run(suite)
